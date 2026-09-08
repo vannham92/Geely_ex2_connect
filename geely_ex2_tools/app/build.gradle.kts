1550803c11bdc01b87dffb7762928c5f0da9d55f
vannham92 <100520404+vannham92@users.noreply.github.com>
2026-09-08 15:27:48 +0700

    Fix formatting of archivesName in build.gradle.kts

--- geely_ex2_tools/app/build.gradle.kts
+++ geely_ex2_tools/app/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
 }
-base { archivesName.set("ex2_car") }
+base {
+    archivesName.set("ex2_car")
+}
 import java.util.Properties
 
 android {
